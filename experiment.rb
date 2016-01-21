4b2965827883ece0226ccade6e44791a4234d6f9
Paweł Ksieniewicz <pawel@ksienie.com>
2016-01-21 02:04:00 +0100

    Check if there is a reason to process.

--- experiment.rb
+++ experiment.rb
@@ -17,35 +17,51 @@ timeCut = config["timeCut"]
 experimentsCount = generatorsSet.length * classifiersSet.length * chunksSet.length * chunkSizeSet.length * thresholdSet.length * budgetSet.length
 experimentNumber = 0
 
-startsWith = ARGV[0] ? ARGV[0].to_i : 0
-endsWith = ARGV[1] ? ARGV[1].to_i : experimentsCount
+if ARGV[0] && ARGV[0] == "count"
+	puts experimentsCount
+else
+	startsWith = ARGV[0] ? ARGV[0].to_i : 0
+	endsWith = ARGV[1] ? ARGV[1].to_i : experimentsCount
 
-for chunks in chunksSet
-for chunkSize in chunkSizeSet
-for threshold in thresholdSet
-for classifier in classifiersSet
-for generator in generatorsSet
-for budget in budgetSet
-	experimentNumber += 1
-	if(experimentNumber < startsWith || experimentNumber > endsWith) 
-		next
+	for chunks in chunksSet
+	for chunkSize in chunkSizeSet
+	for threshold in thresholdSet
+	for classifier in classifiersSet
+	for generator in generatorsSet
+	for budget in budgetSet
+		experimentNumber += 1
+
+		if(experimentNumber < startsWith || experimentNumber > endsWith) 
+			next
+		end
+
+		classifierName = classifier.split('.').last.downcase
+		generatorName = generator.split('.').last.downcase
+		outputFilename = "#{classifierName}_#{generatorName}_i_#{chunks}_s_#{chunkSize}_t_#{threshold}_b_#{budget}.csv"
+
+		puts "# [#{experimentNumber}/#{experimentsCount}][#{(100*experimentNumber/experimentsCount).round}%]".red
+
+		if File.exists?"results/#{outputFilename}"
+			puts "File exists!".red
+			if !File.size?"results/#{outputFilename}"
+				puts "But it's empty. Processing anyway".yellow
+			else
+				next
+			end
+		end
+		
+		command = "java -cp moa.jar -javaagent:sizeofag.jar moa.DoTask \"EvaluatePrequentialActive -l #{classifier} -s #{generator} -i #{chunks} -b #{budget} -r #{threshold} -c #{chunkSize} -t #{timeCut}\" > results/#{outputFilename}"
+		puts "# Testing #{classifierName} on #{generatorName}".yellow
+		puts "# #{chunks} chunks #{chunkSize} instances each, on #{threshold} threshold and #{budget} budget".green
+
+		puts "#{format("%.3f", experimentNumber.to_f / experimentsCount.to_f)}"
+		open("http://156.17.43.89:8888/moaStatus/#{format("%.3f", experimentNumber.to_f / experimentsCount.to_f)}").read
+
+		system(command)
 	end
-
-	classifierName = classifier.split('.').last.downcase
-	generatorName = generator.split('.').last.downcase
-	outputFilename = "#{classifierName}_#{generatorName}_i_#{chunks}_s_#{chunkSize}_t_#{threshold}_b_#{budget}.csv"
-	command = "java -cp moa.jar -javaagent:sizeofag.jar moa.DoTask \"EvaluatePrequentialActive -l #{classifier} -s #{generator} -i #{chunks} -b #{budget} -r #{threshold} -c #{chunkSize} -t #{timeCut}\" > results/#{outputFilename}"
-	puts "# [#{experimentNumber}/#{experimentsCount}][#{(100*experimentNumber/experimentsCount).round}%]".red
-	puts "# Testing #{classifierName} on #{generatorName}".yellow
-	puts "# #{chunks} chunks #{chunkSize} instances each, on #{threshold} threshold and #{budget} budget".green
-
-	puts "#{format("%.3f", experimentNumber.to_f / experimentsCount.to_f)}"
-	open("http://156.17.43.89:8888/moaStatus/#{format("%.3f", experimentNumber.to_f / experimentsCount.to_f)}").read
-
-	system(command)
-end
-end
-end
-end
-end
+	end
+	end
+	end
+	end
+	end
 end
